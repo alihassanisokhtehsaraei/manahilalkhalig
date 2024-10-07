ae1b71ccbeda26d4897f79acb7f6197d1ff98a1d
Saeed <ssarailoo@yahoo.com>
2024-10-07 12:54:21 +0330

    feat: add Lab Fee management to layout

--- Modules/Inspection/Http/Controllers/LabFeeController.php
+++ Modules/Inspection/Http/Controllers/LabFeeController.php
@@ -72,11 +72,7 @@ class LabFeeController extends Controller
     public function search(Request $request)
     {
         $query = $request->get('query');
-
-        // Search the LabFee model by English name
         $labFees = LabFee::where('english_name', 'LIKE', "%{$query}%")->get(['id', 'english_name', 'arabic_name', 'fee', 'category']);
-
-        // Return the data as JSON
         return response()->json($labFees);
     }
 }
